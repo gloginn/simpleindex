39043a849e2fe97c5a4276ad4b5270d512e96d6d
gloginn <g.martins.contato@gmail.com>
2020-10-27 15:11:58 -0300

    .

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+echo "<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='UTF-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+  <title>Worked!</title>
+</head>
+
+<body>
+{$_SERVER['SERVER_ADDR']};
+</body>
+
+</html>";
